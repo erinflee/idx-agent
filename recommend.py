fc7da6aab39462b838a4bb48f43c3f7c913cac29
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-03 14:21:10 -0700

    initialize score to 0 before using conditional statements

--- recommend.py
+++ recommend.py
@@ -6,7 +6,10 @@ Structured score + embedding similarity. Thin TS skill calls over HTTP.
 
 
 def calculate_similarity_score(target, candidate): 
+
     price_diff = target["price"] - candidate["price"]
+    score = 0
+
     if price_diff < 50000: score += 20
     elif price_diff < 150000: score += 12
     elif price_diff < 300000: score += 5
