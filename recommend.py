625ae3b8103fef5a638eefbc56d28184d831e31b
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-04 16:24:34 -0700

    fix variable naming convention to store correct variable

--- recommend.py
+++ recommend.py
@@ -101,6 +101,6 @@ def get_recommendations(listing_id, k=5): # listing_id is what user likes and wa
         candidate_emb = get_embedding_by_id(i)
         score = calculate_similarity_score(target, candidate, target_emb, candidate_emb)
         comp = validate_with_comps(candidate["city"], candidate["sqft"], candidate["price"])
-        results.append({**candidate, "score": score, "comp": comps})
+        results.append({**candidate, "score": score, "comp": comp})
 
     return results
