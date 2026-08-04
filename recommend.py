328443ce409b2dc6a76a88ff180427270d227e19
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-04 16:20:15 -0700

    fix uninitialized candidate variable by calling get_embedding_by_id()

--- recommend.py
+++ recommend.py
@@ -98,6 +98,7 @@ def get_recommendations(listing_id, k=5): # listing_id is what user likes and wa
 
     for i in top_k_ids:
         candidate = dicts[i] 
+        candidate_emb = get_embedding_by_id(i)
         score = calculate_similarity_score(target, candidate, target_emb, candidate_emb)
         comp = validate_with_comps(city, sqft, price)
         results.append({candidate, "score": score, "comp": comps})
